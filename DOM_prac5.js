bc5651d667631cc08f52c7a76e68878c7315e93d
hyunjun9788 <namhyunjun9788@gmail.com>
2023-05-22 15:00:00 +0900

    feat: 모달창 생성

---
+++ DOM_prac5.js
@@ -0,0 +1,9 @@
+function openModal(){
+    const modal = document.getElementById('modal');
+    modal.style.display = 'flex'
+}
+
+function closeModal(){
+    const modal = document.getElementById('modal');
+    modal.style.display='none'
+}
